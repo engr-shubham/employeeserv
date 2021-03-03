c0be94fcdd071a8c745e5b73d901f57005a8d132
engr-shubham <79952238+engr-shubham@users.noreply.github.com>
2021-03-03 07:46:20 +0530

    Added code for employee management application.

---
+++ employeeserv-master/employeeserv-master/employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.paypal.bfs.test.employeeserv;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.paypal.bfs.test.employeeserv.api.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {}
