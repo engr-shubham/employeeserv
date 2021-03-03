40e0bc3ddb183f160fe030efd9b317d39961be92
engr-shubham <79952238+engr-shubham@users.noreply.github.com>
2021-03-03 08:12:09 +0530

    Delete employeeserv-master directory

--- employeeserv-master/employeeserv-master/employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paypal.bfs.test.employeeserv;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.paypal.bfs.test.employeeserv.api.model.Employee;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {}
